fc7ae9c8b6fb743fb6a7b15ca631e5102a33ebc3
Lextrack <arielmunozg97@gmail.com>
2024-09-29 15:12:36 -0300

    remove comments

--- Entities/entities_scripts/player_delta.gd
+++ Entities/entities_scripts/player_delta.gd
@@ -2,5 +2,5 @@ extends Node
 
 class_name player_data
 
-static var health: float = 4.0  # Cambia a float y define como 4.0
-static var ammo: int = 100  # Puedes dejarlo como int
+static var health: float = 4.0
+static var ammo: int = 100 
