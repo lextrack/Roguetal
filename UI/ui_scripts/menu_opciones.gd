d89587927a4c14ff484fcace5c0e726df60e8814
lextrack <arielmunozg07@gmail.com>
2024-10-27 21:15:38 -0300

    Combobox resolution was added

--- UI/ui_scripts/menu_opciones.gd
+++ UI/ui_scripts/menu_opciones.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var volumen_slider = $PanelOpciones/VBoxContainer/VolumenSlider
 @onready var fullscreen_check = $PanelOpciones/VBoxContainer/FullscreenCheck
+@onready var resolution_option = $PanelOpciones/VBoxContainer/ResolutionOption
 @onready var language_button = $PanelOpciones/VBoxContainer/LanguageButton
 @onready var panel_opciones = $PanelOpciones
 @onready var options_menu: Label = $PanelOpciones/VBoxContainer/Options
@@ -14,11 +15,20 @@ var has_unsaved_changes = false
 var current_selection = 0
 var focusable_elements = []
 
+var available_resolutions = [
+	Vector2i(1920, 1080),
+	Vector2i(1600, 900),
+	Vector2i(1366, 768),
+	Vector2i(1280, 720),
+	Vector2i(1024, 576)
+]
+
 func _ready():
 	if !_verify_required_nodes():
 		return
 	
 	_setup_volume_slider()
+	_setup_resolution_option() 
 	_connect_signals()
 	load_config()
 	update_language_button_text()
@@ -28,6 +38,7 @@ func _ready():
 		volumen_slider,
 		fullscreen_check,
 		language_button,
+		resolution_option,
 		button_save,
 		button_cancel
 	]
@@ -109,12 +120,19 @@ func _on_element_focused() -> void:
 			current_selection = i
 			break
 
+func _setup_resolution_option() -> void:
+	if resolution_option:
+		resolution_option.clear()
+		for resolution in available_resolutions:
+			resolution_option.add_item("%dx%d" % [resolution.x, resolution.y])
+
 func _verify_required_nodes() -> bool:
 	var required_nodes = {
+		"Options Label": options_menu,
 		"VolumenSlider": volumen_slider,
 		"FullscreenCheck": fullscreen_check,
+		"ResolutionOption": resolution_option,
 		"LanguageButton": language_button,
-		"Options Label": options_menu,
 		"Volume Label": volume_label_game,
 		"Save Button": button_save,
 		"Cancel Button": button_cancel
@@ -135,6 +153,25 @@ func _connect_signals() -> void:
 	
 	if !language_button.pressed.is_connected(_on_language_button_pressed):
 		language_button.pressed.connect(_on_language_button_pressed)
+	
+	# Nueva conexión para el selector de resoluciones
+	if resolution_option and !resolution_option.item_selected.is_connected(_on_resolution_selected):
+		resolution_option.item_selected.connect(_on_resolution_selected)
+
+# Nueva función para manejar el cambio de resolución
+func _on_resolution_selected(index: int) -> void:
+	if index >= 0 && index < available_resolutions.size():
+		var new_resolution = available_resolutions[index]
+		if !fullscreen_check.button_pressed:
+			get_window().mode = Window.MODE_WINDOWED
+			get_window().size = new_resolution
+			# Centrar la ventana
+			var screen_size = DisplayServer.screen_get_size()
+			get_window().position = Vector2i(
+				(screen_size.x - new_resolution.x) / 2,
+				(screen_size.y - new_resolution.y) / 2
+			)
+	_on_setting_changed()
 
 func _setup_volume_slider() -> void:
 	volumen_slider.min_value = 0.0
@@ -143,10 +180,11 @@ func _setup_volume_slider() -> void:
 	volumen_slider.value_changed.connect(_update_volume_label)
 
 func _store_initial_settings() -> void:
-	if volumen_slider and fullscreen_check:
+	if volumen_slider and fullscreen_check and resolution_option:
 		initial_settings = {
 			"volumen": volumen_slider.value,
 			"fullscreen": fullscreen_check.button_pressed,
+			"resolution": resolution_option.selected,
 			"language": TranslationManager.current_language
 		}
 
@@ -157,6 +195,7 @@ func _check_for_changes() -> bool:
 	return (
 		initial_settings["volumen"] != volumen_slider.value or
 		initial_settings["fullscreen"] != fullscreen_check.button_pressed or
+		initial_settings["resolution"] != resolution_option.selected or
 		initial_settings["language"] != TranslationManager.current_language
 	)
 
@@ -178,9 +217,18 @@ func _on_volumen_slider_value_changed(value: float) -> void:
 
 func _on_fullscreen_check_toggled(button_pressed: bool) -> void:
 	if button_pressed:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		get_window().mode = Window.MODE_FULLSCREEN
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		get_window().mode = Window.MODE_WINDOWED
+		# Aplicar la resolución actual cuando salimos de pantalla completa
+		var current_res = available_resolutions[resolution_option.selected]
+		get_window().size = current_res
+		# Centrar la ventana
+		var screen_size = DisplayServer.screen_get_size()
+		get_window().position = Vector2i(
+			(screen_size.x - current_res.x) / 2,
+			(screen_size.y - current_res.y) / 2
+		)
 	_on_setting_changed()
 
 func _on_language_button_pressed() -> void:
@@ -212,14 +260,14 @@ func _input(event: InputEvent) -> void:
 			hide()
 
 func _show_discard_changes_dialog() -> void:
-	# Aquí podrías mostrar un diálogo de confirmación
-	print("¿Deseas descartar los cambios?")
+	pass
 
 func save_config() -> void:
 	var config = ConfigFile.new()
 	
 	config.set_value("audio", "volumen", volumen_slider.value)
 	config.set_value("video", "fullscreen", fullscreen_check.button_pressed)
+	config.set_value("video", "resolution_index", resolution_option.selected)
 	config.set_value("language", "current", TranslationManager.current_language)
 	
 	var err = config.save("user://configuraciones.cfg")
@@ -240,27 +288,62 @@ func load_config() -> void:
 		_apply_default_config()
 
 func _apply_config(config: ConfigFile) -> void:
-	if volumen_slider:
-		var vol = config.get_value("audio", "volumen", 1.0)
-		volumen_slider.value = vol
-		_on_volumen_slider_value_changed(vol)
+	# Primero configuramos la resolución y el modo de pantalla
+	if resolution_option:
+		var res_index = config.get_value("video", "resolution_index", 0)
+		resolution_option.selected = clamp(res_index, 0, available_resolutions.size() - 1)
 	
 	if fullscreen_check:
 		var fs = config.get_value("video", "fullscreen", false)
 		fullscreen_check.button_pressed = fs
-		_on_fullscreen_check_toggled(fs)
+		
+		if fs:
+			get_window().mode = Window.MODE_FULLSCREEN
+		else:
+			get_window().mode = Window.MODE_WINDOWED
+			var current_res = available_resolutions[resolution_option.selected]
+			get_window().size = current_res
+			# Centrar la ventana
+			var screen_size = DisplayServer.screen_get_size()
+			get_window().position = Vector2i(
+				(screen_size.x - current_res.x) / 2,
+				(screen_size.y - current_res.y) / 2
+			)
+	
+	# Configuraciones de audio y lenguaje
+	if volumen_slider:
+		var vol = config.get_value("audio", "volumen", 1.0)
+		volumen_slider.value = vol
+		_on_volumen_slider_value_changed(vol)
 	
 	var saved_language = config.get_value("language", "current", "en")
 	TranslationManager.set_language(saved_language)
 	update_language_button_text()
 
 func _apply_default_config() -> void:
+	# Configurar resolución por defecto
+	if resolution_option:
+		resolution_option.selected = 0
+	
+	# Configurar modo ventana por defecto
+	if fullscreen_check:
+		fullscreen_check.button_pressed = false
+		get_window().mode = Window.MODE_WINDOWED
+		
+		var default_res = available_resolutions[0]
+		get_window().size = default_res
+		# Centrar la ventana
+		var screen_size = DisplayServer.screen_get_size()
+		get_window().position = Vector2i(
+			(screen_size.x - default_res.x) / 2,
+			(screen_size.y - default_res.y) / 2
+		)
+	
+	# Configuraciones de audio y lenguaje
 	if volumen_slider:
 		volumen_slider.value = 1.0
 		_on_volumen_slider_value_changed(1.0)
-	if fullscreen_check:
-		fullscreen_check.button_pressed = false
-		_on_fullscreen_check_toggled(false)
+	
 	TranslationManager.set_language("en")
 	update_language_button_text()
 
