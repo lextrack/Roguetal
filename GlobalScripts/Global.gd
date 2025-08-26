ce0510003146a1750656e479e58f78325d610e6b
Lextrack <arielmunozg97@gmail.com>
2025-08-25 22:23:11 -0400

    Adjust fps limit and fix shotgun sprite

--- GlobalScripts/Global.gd
+++ GlobalScripts/Global.gd
@@ -1,8 +1,18 @@
 extends Node
 
 var camera = null
-
 var has_shown_intro = false
+var graphics_configured = false
+
+func _ready():
+	if not graphics_configured:
+		configure_graphics()
+
+func configure_graphics():
+	DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	Engine.max_fps = 120
+	graphics_configured = true
 
 func reset():
 	has_shown_intro = false
+   
